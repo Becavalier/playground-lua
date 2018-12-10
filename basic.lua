c34d89bb80175df0fbe3a34c7fa6b1651b1340ab
Jason <yhorg@hotmail.com>
2018-12-10 18:09:07 +0800

    chore: update cases.

--- basic.lua
+++ basic.lua
@@ -1,12 +1,23 @@
+#!/usr/local/bin/lua
+print(arg)  -- a global argument includes all params passed to "lua", start from index 0;
+
 print "Hello World!"
 myVar = 10
 print(myVar)
 
 --[[
 This is multi-line comment;
-]]--
+]]
 
 -- This is single-line comment;
+
+---[[
+    print('ABC')
+--]]
+
+--[[
+    print('DEF')
+--]]
 
 myLongText = [[
     apple banana
@@ -67,3 +78,20 @@ repeat
     i = i - 1
     print("Repeat:", i)
 until(i == 0)
+
+function max(x, y)
+    return x > y and x or y
+end
+print(max(1, 2))
+
+print(3 // 2)  -- floor division;
+
+x = math.pi
+print(x - x % 0.01)  -- keep the last 2 bits of the decimal part of the previous float x;
+print(x - x % 0.001)
+
+math.randomseed(os.time())
+print(math.random(10)) -- [1, 10)
+print(math.random()) -- [0, 1)
+
+x, y = math.modf(10.9) -- x:10, y:0.9
